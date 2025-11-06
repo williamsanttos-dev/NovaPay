3902c97e9a5766cb0a3d07864672b9dd82dd88fe
William01br <williamstcorreia123@gmail.com>
2025-11-06 06:41:48 -0300

    feat: add tx, chain, and verify endpoints and add requests in server.http

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import * as legder from "./ledger.js";
 
 const PORT = process.env.PORT || 3002;
 
@@ -8,6 +9,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("json spaces", 2);
+
+app.post("/tx", (req, res) => {
+  const { from, to, amount, meta, signature } = req.body;
+  if (!from || !to || amount == null)
+    return res.status(400).json({
+      error: "from, to, and amount are required",
+    });
+
+  const result = legder.addTransactionAndBlock({
+    from,
+    to,
+    amount,
+    meta,
+    signature,
+  });
+
+  return res.status(200).json(result);
+});
+
+app.get("/chain", (req, res) => {
+  res.status(200).json(legder.getChain());
+});
+
+app.get("/verify", (req, res) => {
+  res.status(200).json(legder.verifyChain());
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json("OK");
